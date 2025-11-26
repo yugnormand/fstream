4cb9783d0bd8c4af1ba24d221490d54636182552
yugnormand <yugnormandngangue@gmail.com>
2025-11-26 23:14:57 +0100

    Remove submodule and add folder as normal

---
+++ plugin.video.fstream/setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# https://github.com/Kodi-vStream/venom-xbmc-addons
+from setuptools import setup, find_packages
+
+setup(
+    name='fStream',
+    version='1.0.0',
+    packages=find_packages(),
+
+    # Declare your packages' dependencies here, for eg:
+    install_requires=['foo>=3'],
+
+    # Fill in these to make your Egg ready for upload to
+    # PyPI
+    author='Lomixxx',
+    author_email='',
+
+    summary='Just another Python package for the cheese shop',
+    url='',
+    license='',
+    long_description='Long description of the package',
+
+    # could also include long_description, download_url, classifiers, etc.
+)
