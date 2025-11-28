7ded4375e7405751383ac5bc62c1f5663ce410d2
yugnormand <yugnormandngangue@gmail.com>
2025-11-28 02:12:12 +0100

    update

--- repo/plugin.video.fstream/resources/lib/gui/menuDelViewing.py
+++
@@ -1,2 +0,0 @@
-import xbmc
-xbmc.executebuiltin("RunPlugin(plugin://plugin.video.fstream/?site=cViewing&function=delViewingMenu)", True)
--- repo/plugin.video.fstream/resources/lib/gui/menuSimil.py
+++
@@ -1,2 +0,0 @@
-import xbmc
-xbmc.executebuiltin("RunPlugin(plugin://plugin.video.fstream/?site=cGui&function=viewSimil)", True)
